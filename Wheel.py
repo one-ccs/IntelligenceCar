35e22b364396efa3ea7fbc52c4721e323de24f09
one-ccs <one-ccs@foxmail.com>
2022-05-14 18:18:19 +0800

    1、增加 Wheel.WheelSystem.active_state 属性 2、增加 Car.Car.turn_left 左转指定度数 3、增加 Car.Car.turn_right 右转指定度数 4、添加文件 UML 类图.svg 4、添加文件 README.md
    Signed-off-by: one-ccs <one-ccs@foxmail.com>

--- Wheel.py
+++ Wheel.py
@@ -15,9 +15,13 @@ from gpiozero import Motor
 
 
 class WheelSystem():
-    """四轮驱动器"""
-    def __init__(self, speed=5):
-        self._speed = speed
+    """四轮驱动器
+    
+    :参数 浮点型 speed:
+        将电机的转速表示为 -1 (全速后退) 到 1 (全速前进) 之间的浮点值.
+    """
+    def __init__(self, speed=0.5):
+        self._speed = speed     # 轮子转动速度百分比
 
         self.left_front_wheel = Motor(self._speed)  # 左前轮
         self.right_front_wheel = Motor(self._speed) # 右前轮
@@ -31,8 +35,25 @@ class WheelSystem():
     @speed.setter
     def speed(self, speed):
         if not isinstance(speed, int):
-            return
+            raise ValueError("")
         self._speed = speed
+
+        self.left_front_wheel.value = speed
+        self.right_front_wheel.value = speed
+        self.right_rear_wheel.value = speed
+        self.left_rear_wheel.value = speed
+
+    @property
+    def active_state(self):
+        """
+        返回一个表示四个轮子 (左前、右前、右后、左后) 运行状态的元组, 元素类型为 bool.
+        """
+        return (
+            self.left_front_wheel.is_active,
+            self.right_front_wheel.is_active,
+            self.right_rear_wheel.is_active,
+            self.left_rear_wheel.is_active
+        )
 
     def stop(self):
         """停止"""
