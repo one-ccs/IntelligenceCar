2d77c4c10a6a36c6c5c9abe9914074fcaf57a719
one-ccs <one-ccs@foxmail.com>
2022-05-14 18:18:19 +0800

    1、增加 Wheel.WheelSystem.active_state 属性 2、增加 Car.Car.turn_left 左转指定度数 3、增加 Car.Car.turn_right 右转指定度数 4、添加文件 UML 类图.svg 4、添加文件 README.md
    Signed-off-by: one-ccs <one-ccs@foxmail.com>

--- Car.py
+++ Car.py
@@ -6,6 +6,8 @@
 #  Copyright 2022 ONE-CCS <ONE-CCS@ONE-CCS>
 #
 # import RPi.GPIO as GPIO
+from time import sleep
+
 from Wheel import WheelSystem
 from Camera import CameraSystem
 from Sensors import InfraredSensor
@@ -16,11 +18,29 @@ from Sensors import TrackingSensor
 class Car():
     """智能小车"""
     def __init__(self):
+        self._STEER_TIME = 0.36 # 车子旋转一度需要的秒数
+        
         self.wheels = WheelSystem()          # 车轮系统
         self.camera = CameraSystem()         # 摄像头
         self.infrared = InfraredSensor()     # 红外避障
         self.ultrasonic = UltrasonicSensor() # 超声波
         self.track = TrackingSensor()        # 寻迹
+
+    def turn_left(self, deg):
+        """
+        向左旋转指定度数.
+        """
+        self.wheels.turn_left()
+        sleep(self._STEER_TIME * deg)
+        self.wheels.stop()
+
+    def turn_right(self, deg):
+        """
+        向右旋转指定度数.
+        """
+        self.wheels.turn_right()
+        sleep(self._STEER_TIME * deg)
+        self.wheels.stop()
 
     def track_demo(self):
         """寻迹演示"""
