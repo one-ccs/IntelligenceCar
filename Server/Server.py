b4841ac5c785533e47b21c713ceed5d380bc499c
one-ccs <one-ccs@foxmail.com>
2022-05-23 12:10:46 +0800

    1、增加网页端超声波 2、增加网页端视频 3、修改主页 4、修改服务器

--- Server/Server.py
+++ Server/Server.py
@@ -3,27 +3,46 @@
 #
 #  Server.py
 #
-from flask import Flask, request
+from IntelligenceCar.Functions import *
+from flask import Flask, request, Response
 from flask import render_template
+import cv2
 
 from sys import path
 path.append(__file__[:-17])
-from IntelligenceCar.Functions import *
 
-ic_server = Flask(__name__)
+PI = Flask(__name__)
 
 
-@ic_server.route("/")
+class VideoCamera(object):
+    def __init__(self):
+        # 通过opencv获取实时视频流
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        success, image = self.video.read()
+        # 在这里处理视频帧
+        cv2.putText(image, "hello", (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0))
+        # 因为opencv读取的图片并非jpeg格式，因此要用motion JPEG模式需要先将图片转码成jpg格式图片
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+
+@PI.route("/")
 def main():
     return render_template("RaspberryPi.html")  # 返回主界面
 
 
-@ic_server.route("/horn", methods=["POST", "GET"])  # 返回轨迹界面
+@PI.route("/horn", methods=["POST", "GET"])  # 返回轨迹界面
 def horn():
     return render_template("Horn.html")
 
 
-@ic_server.route("/getHorn", methods=["POST", "GET"])  # 获取轨迹数据
+@PI.route("/getHorn", methods=["POST", "GET"])  # 获取轨迹数据
 def getHorn():
     if request.method == "POST":
         data = request.get_json()
@@ -55,4 +74,35 @@ def getHorn():
         return "不支持该请求方式"
 
 
-ic_server.run()
+@PI.route('/Vedio')
+def index():
+    # jinja2模板，具体格式保存在index.html文件中
+    return render_template('Vedio.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        # 使用generator函数输出视频流， 每次请求输出的content类型是image/jpeg
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@PI.route('/video_feed')  # 这个地址返回视频流响应
+def video_feed():
+    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@PI.route("/ultrasonic")
+def ultrasonic():
+    return render_template("ultrasonic.html")
+
+
+@PI.route("/showDistance", methods=['POST', 'GET'])
+def showDistance():
+    # dis=距离
+    dis = 0
+    return str(dis)
+
+
+PI.run()
